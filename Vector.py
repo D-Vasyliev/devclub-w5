4874ec4721d947d2f0cc3c990a5048853428525e
D-Vasyliev <zaec20074@gmail.com>
2024-11-10 17:13:25 +0200

    Update Vector.py

--- Vector.py
+++ Vector.py
@@ -2,8 +2,24 @@ import math
 
 class Vector:
     def __init__(self, x=0.0, y=0.0):
-        self.x = x
-        self.y = y
+        self._x = float(x)
+        self._y = float(y)
+
+    @property
+    def x(self):
+        return self._x
+
+    @x.setter
+    def x(self, value):
+        self._x = float(value)
+
+    @property
+    def y(self):
+        return self._y
+
+    @y.setter
+    def y(self, value):
+        self._y = float(value)
 
     def __str__(self):
         return f'({self.x}, {self.y})'
@@ -12,25 +28,38 @@ class Vector:
         return f'Vector({self.x}, {self.y})'
 
     def __eq__(self, other):
-        return self.x == other.x and self.y == other.y
+        if not isinstance(other, self.__class__):
+            return NotImplemented
+        return self.x == other._ and self.y == other.y
 
     def __add__(self, other):
+        if not isinstance(other, self.__class__):
+            return NotImplemented
         return Vector(self.x + other.x, self.y + other.y)
 
     def __sub__(self, other):
+        if not isinstance(other, self.__class__):
+            return NotImplemented
         return Vector(self.x - other.x, self.y - other.y)
 
     def __iadd__(self, other):
+        if not isinstance(other, self.__class__):
+            return NotImplemented
         self.x += other.x
         self.y += other.y
         return self
 
     def __isub__(self, other):
+        if not isinstance(other, self.__class__):
+            return NotImplemented
         self.x -= other.x
         self.y -= other.y
         return self
 
     def distance(self, other):
+        if not isinstance(other, self.__class__):
+            raise ValueError()
+
         return math.hypot(self.x-other.x, self.y-other.y)
 
 
