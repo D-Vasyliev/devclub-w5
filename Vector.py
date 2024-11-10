4b226fc96589e8ae58627ac023513af9927d4fcd
D-Vasyliev <zaec20074@gmail.com>
2024-11-10 14:21:35 +0200

    Create Vector.py

---
+++ Vector.py
@@ -0,0 +1,44 @@
+import math
+
+class Vector:
+    def __init__(self, x=0.0, y=0.0):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f'({self.x}, {self.y})'
+
+    def __repr__(self):
+        return f'Vector({self.x}, {self.y})'
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __iadd__(self, other):
+        self.x += other.x
+        self.y += other.y
+        return self
+
+    def __isub__(self, other):
+        self.x -= other.x
+        self.y -= other.y
+        return self
+
+    def length(self):
+        return math.hypot(self.x, self.y)
+
+
+a = Vector(1, 2.3)
+b = Vector(2.9, 4.3)
+
+print(a.__iadd__(b))
+
+print(a.__add__(b))
+
+print(a.__sub__(b))
